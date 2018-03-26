4dca0ade60e687be968afbb2214343ee78852259
Chayakorn Thainkhao <60070013@kmitl.ac.th>
2018-03-26 15:48:18 +0700

    add animation code

--- code/main.c
+++ code/main.c
@@ -15,11 +15,22 @@ SDL_Surface* start_surface ;
 SDL_Texture* start_texture;
 SDL_Surface* background_gamesurface;
 SDL_Texture* background_gametexture;
+SDL_Surface* player_stand_suface;
+SDL_Texture* player_stand_texture;
+SDL_Surface* player_walk1_suface;
+SDL_Texture* player_walk1_texture;
+SDL_Surface* player_walk2_suface;
+SDL_Texture* player_walk2_texture;
+SDL_Surface* player_jump_suface;
+SDL_Texture* player_jump_texture;
+SDL_Texture* player;
 SDL_Renderer* grenderer;
 SDL_Window* window = NULL;
 SDL_Surface* screensurface = NULL;
 SDL_Rect start_position = {1920/2-200/2, 1080/2-100/2, 200, 100};
+SDL_Rect player_position = {300, 710, 80, 110};
 SDL_Texture* display;
+SDL_Texture* player;
 bool game_menu = true;
 bool ingame = false;
 bool init(){
@@ -73,6 +84,38 @@ bool loadmedia(){
         success = false;
     }
     background_gametexture = SDL_CreateTextureFromSurface(grenderer, background_gamesurface);
+
+    player_stand_suface = IMG_Load("../graphic/player_stand.png");
+    if (player_stand_suface == NULL)
+    {
+        printf("load player_stand.png failed %s\n", IMG_GetError());
+        success = false;
+    }
+    player_stand_texture = SDL_CreateTextureFromSurface(grenderer, player_stand_suface);
+
+    player_walk1_suface = IMG_Load("../graphic/player_walk1.png");
+    if (player_walk1_suface == NULL)
+    {
+        printf("load player_walk1.png failed %s\n", IMG_GetError());
+        success = false;
+    }
+    player_walk1_texture = SDL_CreateTextureFromSurface(grenderer, player_walk1_suface);
+
+    player_walk2_suface = IMG_Load("../graphic/player_walk2.png");
+    if (player_walk2_suface == NULL)
+    {
+        printf("load player_walk2.png failed %s\n", IMG_GetError());
+        success = false;
+    }
+    player_walk2_texture = SDL_CreateTextureFromSurface(grenderer, player_walk2_suface);
+
+    player_jump_suface = IMG_Load("../graphic/player_jump.png");
+    if (player_jump_suface == NULL)
+    {
+        printf("load player_jump.png failed %s\n", IMG_GetError());
+        success = false;
+    }
+    player_walk2_texture = SDL_CreateTextureFromSurface(grenderer, player_walk2_suface);
     return success;
 }
 
@@ -86,8 +129,10 @@ int main(int first, char* array[] ){
         }
         else{
             display = background_texture;
+            player = player_stand_texture;
             int quit = 0;
             while (!quit){
+                int player_status = 0;
                 SDL_Event event;
                 while(SDL_PollEvent(&event)) {
                     if (event.type == SDL_QUIT){
@@ -98,7 +143,21 @@ int main(int first, char* array[] ){
                         game_menu = false;
                         ingame = true;
                     }
+                    if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_d){
+                            player = player_walk1_texture;
+
+                            player_position.x += 10;
+                    }
+                    if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_a){
+                            player = player_walk1_texture;
+                            player_position.x -= 10;
+                    }
+                    if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_w){
+                            player = player_jump_texture;
+                            player_position.y += 20;
+                    }
                 }
+
                 SDL_SetRenderDrawColor(grenderer, 0xff, 0xff, 0xff, 0xff);
                 SDL_RenderClear(grenderer);
                 if(game_menu){
@@ -107,6 +166,12 @@ int main(int first, char* array[] ){
                 }
                 else if(ingame){
                     SDL_RenderCopy(grenderer, background_gametexture, NULL, NULL);
+                    SDL_RenderCopy(grenderer, player, NULL, &player_position);
+                    player = player_stand_texture;
+                    if (player_position.x > 1600){
+                        player_position.x = 100;
+                    }
+
                 }
                 SDL_RenderPresent(grenderer);
             }
